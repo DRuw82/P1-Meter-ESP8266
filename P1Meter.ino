9fc71616d3135cef34a881fd055a406d698c193b
mcfurry <f.b.f.schoenmakers@gmail.com>
2020-04-17 20:09:13 +0200

    Update code and README for communication with Homey

--- P1Meter.ino
+++ P1Meter.ino
@@ -1,21 +1,19 @@
-// #include <TimeLib.h>
-#include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
+#include <Homey.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include "CRC16.h"
 
+#define LED 2
+
 //===Change values from here===
-const char* ssid = "WIFISSID";
-const char* password = "PASSWORD";
+const char* ssid = "WIFI_SSID";
+const char* password = "SUPER_SECRET_PASSWORD";
 const char* hostName = "ESPP1Meter";
-const char* domoticzIP = "192.168.1.35";
-const int domoticzPort = 8090;
-const int domoticzGasIdx = 291;
-const int domoticzEneryIdx = 294;
-const bool outputOnSerial = true;
+const bool outputOnSerial = false;
 //===Change values to here===
 
 // Vars to store meter readings
@@ -25,9 +23,16 @@ long mEOLT = 0; //Meter reading Electrics - return low tariff
 long mEOHT = 0; //Meter reading Electrics - return high tariff
 long mEAV = 0;  //Meter reading Electrics - Actual consumption
 long mEAT = 0;  //Meter reading Electrics - Actual return
+
+long mVL1 = 0;  //Meter reading Electrics - Voltage L1
+long mIL1 = 0;  //Meter reading Electrics - Current L1
+long mVL2 = 0;  //Meter reading Electrics - Voltage L2
+long mIL2 = 0;  //Meter reading Electrics - Current L2
+long mVL3 = 0;  //Meter reading Electrics - Voltage L3
+long mIL3 = 0;  //Meter reading Electrics - Current L3
+
 long mGAS = 0;    //Meter reading Gas
 long prevGAS = 0;
-
 
 #define MAXLINELENGTH 128 // longest normal line is 47 char (+3 for \r\n\0)
 char telegram[MAXLINELENGTH];
@@ -37,13 +42,25 @@ SoftwareSerial mySerial(SERIAL_RX, -1, true, MAXLINELENGTH); // (RX, TX. inverte
 
 unsigned int currentCRC=0;
 
-void SendToDomoLog(char* message)
-{
-  char url[512];
-  sprintf(url, "http://%s:%d/json.htm?type=command&param=addlogmessage&message=%s", domoticzIP, domoticzPort, message); 
-}
-
 void setup() {
+  Homey.begin("P1 Smart Meter");
+  Homey.setClass("other");
+  Homey.addCapability("measure_power.consumed");
+  Homey.addCapability("measure_power.produced");
+  Homey.addCapability("meter_power.producedPeak");
+  Homey.addCapability("meter_power.producedOffPeak");
+  Homey.addCapability("meter_power.peak");
+  Homey.addCapability("meter_power.offPeak");
+
+  Homey.addCapability("measure_voltage.L1");
+  Homey.addCapability("measure_current.L1");
+  Homey.addCapability("measure_voltage.L2");
+  Homey.addCapability("measure_current.L2");
+  Homey.addCapability("measure_voltage.L3");
+  Homey.addCapability("measure_current.L3");
+
+  Homey.addCapability("meter_gas");
+
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
@@ -88,56 +105,32 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-
-bool SendToDomo(int idx, int nValue, char* sValue)
-{
-  HTTPClient http;
-  bool retVal = false;
-  char url[255];
-  sprintf(url, "http://%s:%d/json.htm?type=command&param=udevice&idx=%d&nvalue=%d&svalue=%s", domoticzIP, domoticzPort, idx, nValue, sValue);
-  Serial.printf("[HTTP] GET... URL: %s\n",url);
-  http.begin(url); //HTTP
-  int httpCode = http.GET();
-  // httpCode will be negative on error
-  if (httpCode > 0)
-  { // HTTP header has been send and Server response header has been handled
-    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-    // file found at server
-    if (httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-      retVal = true;
-    }
-  }
-  else
-  {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
-  http.end();
-  return retVal;
-}
-
-
-
 void UpdateGas()
 {
-  //sends over the gas setting to domoticz
+  //sends over the gas setting to Homey
   if(prevGAS!=mGAS)
   {
-    char sValue[10];
-    sprintf(sValue, "%d", mGAS);
-    if(SendToDomo(domoticzGasIdx, 0, sValue))
-      prevGAS=mGAS;
+    Homey.setCapabilityValue("meter_gas", (float) (mGAS/1000.0));
+    prevGAS=mGAS;
   }
 }
 
 void UpdateElectricity()
 {
-  char sValue[255];
-  sprintf(sValue, "%d;%d;%d;%d;%d;%d", mEVLT, mEVHT, mEOLT, mEOHT, mEAV, mEAT);
-  SendToDomo(domoticzEneryIdx, 0, sValue);
-}
+  Homey.setCapabilityValue("measure_power.consumed", (float) (mEAV*1.0));
+  Homey.setCapabilityValue("measure_power.produced", (float) (mEAT*1.0));
+  Homey.setCapabilityValue("meter_power.producedPeak", (float) (mEOHT*0.001));
+  Homey.setCapabilityValue("meter_power.producedOffPeak", (float) (mEOLT*0.001));
+  Homey.setCapabilityValue("meter_power.peak", (float) (mEVHT*0.001));
+  Homey.setCapabilityValue("meter_power.offPeak", (float) (mEVLT*0.001));
 
+  Homey.setCapabilityValue("measure_voltage.L1", (float) (mVL1*0.001));
+  Homey.setCapabilityValue("measure_current.L1", (float) (mIL1*0.001));
+  Homey.setCapabilityValue("measure_voltage.L2", (float) (mVL2*0.001));
+  Homey.setCapabilityValue("measure_current.L2", (float) (mIL2*0.001));
+  Homey.setCapabilityValue("measure_voltage.L3", (float) (mVL3*0.001));
+  Homey.setCapabilityValue("measure_current.L3", (float) (mIL3*0.001));
+}
 
 bool isNumber(char* res, int len) {
   for (int i = 0; i < len; i++) {
@@ -160,9 +153,9 @@ int FindCharInArrayRev(char array[], char c, int len) {
 long getValidVal(long valNew, long valOld, long maxDiffer)
 {
   //check if the incoming value is valid
-      if(valOld > 0 && ((valNew - valOld > maxDiffer) && (valOld - valNew > maxDiffer)))
-        return valOld;
-      return valNew;
+  if(valOld > 0 && ((valNew - valOld > maxDiffer) && (valOld - valNew > maxDiffer)))
+    return valOld;
+  return valNew;
 }
 
 long getValue(char* buffer, int maxlen) {
@@ -170,7 +163,7 @@ long getValue(char* buffer, int maxlen) {
   if (s < 8) return 0;
   if (s > 32) s = 32;
   int l = FindCharInArrayRev(buffer, '*', maxlen - 2) - s - 1;
-  if (l < 4) return 0;
+  if (l < 3) return 0;
   if (l > 12) return 0;
   char res[16];
   memset(res, 0, sizeof(res));
@@ -194,15 +187,15 @@ bool decodeTelegram(int len) {
     currentCRC=CRC16(0x0000,(unsigned char *) telegram+startChar, len-startChar);
     if(outputOnSerial)
     {
-      for(int cnt=startChar; cnt<len-startChar;cnt++)
+      for(int cnt=startChar; cnt<len-startChar;cnt++){
         Serial.print(telegram[cnt]);
-    }    
-    //Serial.println("Start found!");
-    
+      }
+    }
+
   }
   else if(endChar>=0)
   {
-    //add to crc calc 
+    //add to crc calc
     currentCRC=CRC16(currentCRC,(unsigned char*)telegram+endChar, 1);
     char messageCRC[5];
     strncpy(messageCRC, telegram + endChar + 1, 4);
@@ -211,10 +204,11 @@ bool decodeTelegram(int len) {
     {
       for(int cnt=0; cnt<len;cnt++)
         Serial.print(telegram[cnt]);
-    }    
+    }
+
     validCRCFound = (strtol(messageCRC, NULL, 16) == currentCRC);
     if(validCRCFound)
-      Serial.println("\nVALID CRC FOUND!"); 
+      Serial.println("\nVALID CRC FOUND!");
     else
       Serial.println("\n===INVALID CRC FOUND!===");
     currentCRC = 0;
@@ -233,42 +227,67 @@ bool decodeTelegram(int len) {
   long val2=0;
   // 1-0:1.8.1(000992.992*kWh)
   // 1-0:1.8.1 = Elektra verbruik laag tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:1.8.1", strlen("1-0:1.8.1")) == 0) 
+  if (strncmp(telegram, "1-0:1.8.1", strlen("1-0:1.8.1")) == 0)
     mEVLT =  getValue(telegram, len);
-  
 
   // 1-0:1.8.2(000560.157*kWh)
   // 1-0:1.8.2 = Elektra verbruik hoog tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:1.8.2", strlen("1-0:1.8.2")) == 0) 
+  if (strncmp(telegram, "1-0:1.8.2", strlen("1-0:1.8.2")) == 0)
     mEVHT = getValue(telegram, len);
-    
 
   // 1-0:2.8.1(000348.890*kWh)
   // 1-0:2.8.1 = Elektra opbrengst laag tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:2.8.1", strlen("1-0:2.8.1")) == 0) 
+  if (strncmp(telegram, "1-0:2.8.1", strlen("1-0:2.8.1")) == 0)
     mEOLT = getValue(telegram, len);
-   
 
   // 1-0:2.8.2(000859.885*kWh)
   // 1-0:2.8.2 = Elektra opbrengst hoog tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:2.8.2", strlen("1-0:2.8.2")) == 0) 
+  if (strncmp(telegram, "1-0:2.8.2", strlen("1-0:2.8.2")) == 0)
     mEOHT = getValue(telegram, len);
-    
 
   // 1-0:1.7.0(00.424*kW) Actueel verbruik
   // 1-0:2.7.0(00.000*kW) Actuele teruglevering
   // 1-0:1.7.x = Electricity consumption actual usage (DSMR v4.0)
-  if (strncmp(telegram, "1-0:1.7.0", strlen("1-0:1.7.0")) == 0) 
+  if (strncmp(telegram, "1-0:1.7.0", strlen("1-0:1.7.0")) == 0)
     mEAV = getValue(telegram, len);
-    
+
   if (strncmp(telegram, "1-0:2.7.0", strlen("1-0:2.7.0")) == 0)
     mEAT = getValue(telegram, len);
-   
 
   // 0-1:24.2.1(150531200000S)(00811.923*m3)
   // 0-1:24.2.1 = Gas (DSMR v4.0) on Kaifa MA105 meter
-  if (strncmp(telegram, "0-1:24.2.1", strlen("0-1:24.2.1")) == 0) 
+  if (strncmp(telegram, "0-1:24.2.1", strlen("0-1:24.2.1")) == 0)
     mGAS = getValue(telegram, len);
+
+  // 1-0:32.7.0 (V)
+  // 1-0:32.7.0 = Instantaan voltage L1 (DSMR v4.0)
+  if (strncmp(telegram, "1-0:32.7.0", strlen("1-0:32.7.0")) == 0)
+    mVL1 = getValue(telegram, len);
+
+  // 1-0:52.7.0 (V)
+  // 1-0:52.7.0 = Instantaan voltage L2 (DSMR v4.0)
+  if (strncmp(telegram, "1-0:52.7.0", strlen("1-0:52.7.0")) == 0)
+    mVL2 = getValue(telegram, len);
+
+  // 1-0:72.7.0 (V)
+  // 1-0:72.7.0 = Instantaan voltage L3 (DSMR v4.0)
+  if (strncmp(telegram, "1-0:72.7.0", strlen("1-0:72.7.0")) == 0)
+    mVL3 = getValue(telegram, len);
+
+  // 1-0:31.7.0 (I)
+  // 1-0:31.7.0 = Instantaan current L1 (DSMR v4.0)
+  if (strncmp(telegram, "1-0:31.7.0", strlen("1-0:31.7.0")) == 0)
+    mIL1 = getValue(telegram, len);
+
+  // 1-0:51.7.0 (I)
+  // 1-0:51.7.0 = Instantaan current L2 (DSMR v4.0)
+  if (strncmp(telegram, "1-0:51.7.0", strlen("1-0:51.7.0")) == 0)
+    mIL2 = getValue(telegram, len);
+
+  // 1-0:71.7.0 (I)
+  // 1-0:71.7.0 = Instantaan current L3 (DSMR v4.0)
+  if (strncmp(telegram, "1-0:71.7.0", strlen("1-0:71.7.0")) == 0)
+    mIL3 = getValue(telegram, len);
 
   return validCRCFound;
 }
@@ -281,21 +300,40 @@ void readTelegram() {
       telegram[len] = '\n';
       telegram[len+1] = 0;
       yield();
+
       if(decodeTelegram(len+1))
       {
          UpdateElectricity();
          UpdateGas();
       }
-    } 
+    }
   }
 }
 
-
-
-void loop() {
-  readTelegram();
-  ArduinoOTA.handle();
+void wifi()
+{
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      digitalWrite(LED,!digitalRead(LED));
+      Serial.print(".");
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      //Print IP address
+      Serial.print("Connected to WiFi! (");
+      Serial.print(WiFi.localIP());
+      Serial.println(")");
+    }
+  }
+  digitalWrite(LED, 1);
 }
 
+void loop() {
+  wifi();
 
+  readTelegram();
+  Homey.loop();
 
+  ArduinoOTA.handle();
+}
--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # P1-Meter-ESP8266
-Software for the ESP2866 that sends P1 smart meter data to Domoticz (with CRC checking and OTA firmware updates)
+Software for the ESP8266 that sends P1 smart meter data to Homey (with CRC checking and OTA firmware updates)
+Many thanks to Jan ten Hove by writing this code for Domoticz: https://github.com/jantenhove/P1-Meter-ESP8266
+This repo is a fork from Jan and changed to communicate with Homey instead of Domoticz
 
-### Installation instrucions
+### Installation instructions
 - Make sure that your ESP8266 can be flashed from the Arduino environnment: https://github.com/esp8266/Arduino
 - Install the SoftSerial library from: https://github.com/plerup/espsoftwareserial
-- Place all files from this repository in a directory. Open the .ino file.
-- Adjust WIFI, Domoticz and debug settings at the top of the file
+- Install the HomeyDuino library from: https://github.com/athombv/homey-arduino-library
+- Place all files from this repository in a directory. Open the .ino file with Arduino.
+- Adjust WIFI, Homey and debug settings at the top of the file
 - Compile and flash
+- Install the HomeyDuino App to Homey from: https://homey.app/nl-nl/app/com.athom.homeyduino/Homeyduino/
+- Connect your ESP8266 to the P1 port of your smart meter and power on
+- Add a device to Homey from the HomeyDuino category and select the ESPP1Meter
 
 ### Connection of the P1 meter to the ESP8266
 You need to connect the smart meter with a RJ11 connector. This is the pinout to use
 ![RJ11 P1 connetor](http://gejanssen.com/howto/Slimme-meter-uitlezen/RJ11-pinout.png)
+(You could also use an RJ45 connector and file the sides to make it fit the RJ11 socket)
 
-Connect GND->GND on ESP, RTS->3.3V on ESP and RxD->any digital pin on ESP. In this sketch I use D5
+Connect GND->GND on ESP, RTS->3.3V on ESP and RxD->any digital pin on ESP. In this sketch I use D5.
+When you're equipped with an ESMR5 smart meter, it is possible to power the ESP8266 from the 5V on the P1 port.
